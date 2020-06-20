1b1777ffdf7aa9ad473397daf6c135d80cf2a359
Hafiz Hizers <clickerhizers@gmail.com>
2020-06-21 01:15:57 +0800

    add colors

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -5,3 +5,4 @@ export const PRIMARY_DARK = '#445c6d';
 export const TRANSPARENT = 'transparent';
 export const TEXT_PRIMARY = '#445c6d';
 export const INPUT_TEXT_PRIMARY = '#323232';
+export const GREY = '#808080';
